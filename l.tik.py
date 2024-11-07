750824ae2e63cba1aa234c1a905180cb7b7858bc
ic33ae <icaaapry@gmail.com>
2024-11-07 14:23:06 +0700

    Menambah informasi pada file l.tik.py

--- l.tik.py
+++ l.tik.py
@@ -1,1 +1,6 @@
 print("Selamat "*3)
+
+nama_lengkap = "Ica Apriyanti Rahayu"
+nama_panggilan = "Ica"
+kelas = "1B"
+print(f"perkenalkan aku {nama_lengkap}, biasanya dipanggi {nama_panggilan}, kelas {kelas}")
